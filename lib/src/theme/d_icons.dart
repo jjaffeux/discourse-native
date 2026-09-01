b8a1bd92aaeabd0adfe01f8d3c6f04f98b27747e
Joffrey JAFFEUX <j.jaffeux@gmail.com>
2026-09-02 00:35:43 +0200

    UX: match Chat drawer collapse icon

--- lib/src/theme/d_icons.dart
+++ lib/src/theme/d_icons.dart
@@ -139,6 +139,11 @@ abstract final class DIcons {
     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="currentColor"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 144L48 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l144 0 0 144c0 17.7 14.3 32 32 32s32-14.3 32-32l0-144 144 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-144 0 0-144z"/></svg>',
   );
 
+  static const DIconData minus = DIconData(
+    'minus',
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" fill="currentColor"><path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/></svg>',
+  );
+
   static const DIconData circlePlus = DIconData(
     'circle-plus',
     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344l0-64-64 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l64 0 0-64c0-13.3 10.7-24 24-24s24 10.7 24 24l0 64 64 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-64 0 0 64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>',
@@ -528,6 +533,7 @@ abstract final class DIcons {
     'right-from-bracket': rightFromBracket,
     'shuffle': shuffle,
     'plus': plus,
+    'minus': minus,
     'circle-plus': circlePlus,
     'circle-minus': circleMinus,
     'circle-info': circleInfo,
